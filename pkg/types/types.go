f3e93af91075ce0468e82c84c1a2942a4e7f00cb
Bakhtiyor Aminzoda <hilolon@yandex.ru>
2021-04-06 02:35:12 +0500

    v2

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,17 @@ type Money int64
 // Category представляет собой категорию, в которию был совершен платёж (авто, аптека, рестараны и т.д)
 type Category string
 
+// Status представляет статусы платежеа.
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment представляет игформацию о платиже
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }
